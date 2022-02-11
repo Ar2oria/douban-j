202ae36305346899e35e1bfe3538bb5d9b5297ce
ar2oria <ex1stance@outlook.com>
2022-02-12 00:50:08 +0800

    优化词库

--- src/main/java/cc/w0rm/douban/App.java
+++ src/main/java/cc/w0rm/douban/App.java
@@ -8,7 +8,7 @@ import com.google.common.collect.Lists;
  */
 public class App {
     public static void main(String[] args) {
-        DoubanEngine.run("望京", Lists.newArrayList(), true, 10,
+        DoubanEngine.run("望京", Lists.newArrayList(""), false, 10,
                 "bid=ZSuQFFBm9SY; gr_user_id=71140210-ec3e-4718-93b2-39dffb5296a2; UM_distinctid=17d930f8586156-0370e42e92e805-1f396452-1ea000-17d930f85872ca; douban-fav-remind=1; ll=\"108288\"; viewed=\"26853217_21331443\"; dbcl2=\"223241655:drJoWTERrTg\"; ck=c9A8; ct=y; push_doumail_num=0; push_noty_num=0");
 
         System.exit(0);
