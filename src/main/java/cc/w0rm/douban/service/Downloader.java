bea7550cff1bb6209e62a34dbcba763bfbb2fc65
xuyang57 <xuyang57@meituan.com>
2022-02-11 11:56:39 +0800

    finish - 1

--- src/main/java/cc/w0rm/douban/service/Downloader.java
+++ src/main/java/cc/w0rm/douban/service/Downloader.java
@@ -51,6 +51,7 @@ public class Downloader {
             try {
                 Request req = new Request.Builder()
                         .url(requestBO.getUrl())
+                        .addHeader("Connection", "close")
                         .addHeader("Host", "uz.yurixu.com")
                         .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36")
                         .build();
